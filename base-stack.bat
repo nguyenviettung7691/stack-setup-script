1780515a0f762a52b12dfc8ff8a5b79eba5183d6
Nguyễn Việt Tùng <39761347+nguyenviettung7691@users.noreply.github.com>
2021-05-21 15:07:27 +0700

    Update base-stack.bat

--- base-stack.bat
+++ base-stack.bat
@@ -1,18 +1,6 @@
 @echo off
 
-echo * Ensure that you are running this file as Administrator
-echo;
-pause
-
-echo * Disabling proxies...
-set HTTP_PROXY=
-set HTTPS_PROXY=
-echo;
-
-echo * Installing Chocolatey...
-powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
-choco feature enable -n=allowGlobalConfirmation
-echo;
+call pre-script.bat
 
 echo * Installing Browsers...
 choco install googlechrome.dev
