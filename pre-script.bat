ca9112f266b2e9588fa09f7dfc40a0675e88f8d0
Nguyễn Việt Tùng <39761347+nguyenviettung7691@users.noreply.github.com>
2021-05-21 15:00:39 +0700

    Create pre-script.bat

---
+++ pre-script.bat
@@ -0,0 +1,11 @@
+@echo off
+
+echo * Disabling proxies...
+set HTTP_PROXY=
+set HTTPS_PROXY=
+echo;
+
+echo * Installing Chocolatey...
+powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+choco feature enable -n=allowGlobalConfirmation
+echo;
