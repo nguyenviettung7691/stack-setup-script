32f203c3864ef9b87c2807981c7353bdcfaf0094
Nguyễn Việt Tùng <39761347+nguyenviettung7691@users.noreply.github.com>
2021-05-21 14:56:01 +0700

    Update web-develop-full-stack.bat

--- web-develop-full-stack.bat
+++ web-develop-full-stack.bat
@@ -4,46 +4,12 @@ echo * Ensure that you are running this file as Administrator
 echo;
 pause
 
-echo * Disabling proxies...
-set HTTP_PROXY=
-set HTTPS_PROXY=
-echo;
-
-echo * Installing Chocolatey...
-powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
-choco feature enable -n=allowGlobalConfirmation
-echo;
-
 set /p choice=Do you want to install Base Stack (Browsers, Social App, Essential Apps)? [y/n]
 IF %choice% == y goto Base
 IF %choice% == n goto Dev
 
 :Base
-echo * Installing Browsers...
-choco install googlechrome.dev
-choco install opera-developer
-choco install microsoft-edge
-choco install firefox
-echo;
-
-echo * Installing Social Apps...
-choco install skype
-choco install slack
-choco install microsoft-teams
-choco install zoom
-echo;
-
-echo * Installing Essential Apps...
-choco install paint.net
-choco install inkscape
-choco install figma
-choco install notepadplusplus
-choco install 7zip
-choco install teamviewer
-choco install k-litecodecpackfull
-choco install driverbooster
-choco install greenshot
-echo;
+call base-stack.bat
 
 :Dev
 set /p choice=Do you want to install Dev Stack? [y/n]
@@ -51,36 +17,7 @@ IF %choice% == y goto DevInstall
 IF %choice% == n goto Done
 
 :DevInstall
-echo * Installing Dev Env
-choco install python
-choco install nodejs
-choco install docker-desktop
-echo;
-
-echo * Installing Source control...
-choco install git --params "/GitAndUnixToolsOnPath /NoAutoCrlf /WindowsTerminal"
-choco install git-fork
-echo;
-
-echo * Installing Terminal tools...
-choco install microsoft-windows-terminal
-choco install putty
-echo;
-
-echo * Installing Dev Tools
-choco install vscode
-choco install dbeaver
-echo;
-
-echo * Installing Debug tools...
-choco install postman
-choco install fiddler
-echo;
-
-echo * Installing WSL2 + Ubuntu instance...
-choco install wsl2
-choco install wsl-ubuntu-2004 --params "/InstallRoot:true"
-echo;
+call web-develop-dev-stack.bat
 
 :Done
 echo * Finished!
